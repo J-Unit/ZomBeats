2bd2af2d6e8fa4f33127f043067a3641e31bb4ba
blasterjaxx <mln45@cornell.edu>
2015-03-08 17:03:04 -0400

    Next song with minor changes to songdecomposition

--- Classes/SongDecomposition.cpp
+++ Classes/SongDecomposition.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 
 bool SongDecomposition::isOnBeat(float clickTime){
-	float time_to_first_beat = 0;//.22;//.1564
+	float time_to_first_beat = trackStartTime;//.22;//.1564
 	float mpb = 60 / bpm;
 	float offset_clicktime = clickTime + time_to_first_beat;
 	float clicked = fmod(offset_clicktime,mpb);
