274e2b92942741b715a551dd6c1fa5966b38725c
Matthew Hayes <mjh388@cornell.edu>
2015-03-15 18:08:31 -0400

    working adroid soln part 2

---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+D:\Code\z\ZomBeats\proj.android\gen\com\junit\ZomBeats\R.java \
+ : D:\Code\z\ZomBeats\proj.android\res\drawable-hdpi\icon.png \
+D:\Code\z\ZomBeats\proj.android\res\drawable-ldpi\icon.png \
+D:\Code\z\ZomBeats\proj.android\res\drawable-mdpi\icon.png \
+D:\Code\z\ZomBeats\proj.android\res\values\strings.xml \
+D:\Code\z\ZomBeats\proj.android\bin\AndroidManifest.xml \
